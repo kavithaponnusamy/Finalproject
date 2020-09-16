0e67b006546637fba56fdf67fcb52ddd10f6992c
Mike Cooper <cooperm27@gmail.com>
2020-09-16 11:14:17 -0400

    login for savedsearch

--- src/main/java/co/grandcircus/FinalProject/dao/SavedSearchesDao.java
+++ src/main/java/co/grandcircus/FinalProject/dao/SavedSearchesDao.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import co.grandcircus.FinalProject.entity.SavedSearches;
 
 public interface SavedSearchesDao extends JpaRepository<SavedSearches, Long>{
+
+	SavedSearches findByNameAndUserIdAndSearchUrl(String name, Long id, String searchUrl);
 	
+
 }
 
