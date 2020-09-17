8e0dfb414b4993da5489ec1dd8aaeff50c003eb8
Kavitha <kavithaa434@gmail.com>
2020-09-17 09:06:01 -0400

    sorting

--- src/main/java/co/grandcircus/FinalProject/entity/BuyerInformation.java
+++ src/main/java/co/grandcircus/FinalProject/entity/BuyerInformation.java
@@ -12,6 +12,11 @@ public class BuyerInformation {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String comments;
+	private String quote;
+	public String propertyId;	
+	private String name;
+	private String email;
+	private String phone;
 	@ManyToOne
 	private User user;
 	public Long getId() {
@@ -20,11 +25,6 @@ public class BuyerInformation {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	private String quote;
-	public String propertyId;
-	
-	
-
 	public String getComments() {
 		return comments;
 	}
@@ -56,11 +56,30 @@ public class BuyerInformation {
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 	@Override
 	public String toString() {
 		return "BuyerInformation [id=" + id + ", comments=" + comments + ", quote=" + quote + ", propertyId="
-				+ propertyId + ", user=" + user + "]";
+				+ propertyId + ", name=" + name + ", email=" + email + ", phone=" + phone + ", user=" + user + "]";
 	}
 
 
